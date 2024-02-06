d71ea24f717b2d9ed6b05449ec37b966c0bf9e3f
zhendewokusi <768672249@qq.com>
2024-02-07 00:20:21 +0800

    add makefile

--- OS/真相还原/kernel/main.c
+++ OS/真相还原/kernel/main.c
@@ -3,17 +3,11 @@ int main(void) {
     asm("xchg %bx,%bx");
     // char buff[16] = "I am kernel\n";
     put_str("I am kernel\n");
-    // put_str("I am kernel\n");
     put_char('A');
-//    put_int(0);
-//    put_char('\n');
-//    put_int(9);
-//    put_char('\n');
-//    put_int(0x00021a3f);
-//    put_char('\n');
-//    put_int(0x12345678);
-//    put_char('\n');
-//    put_int(0x00000000);
-//    put_str("I am kernel\n");
+    put_int(9);
+    put_char('\n');
+    put_int(0x12345678);
+    put_char('\n');
+    put_int(0x00000000);
    while(1);
 }
