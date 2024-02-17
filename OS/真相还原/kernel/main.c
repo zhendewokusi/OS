dc75310e11f6bcddc62b647e38ef96d25336505f
zhendewokusi <768672249@qq.com>
2024-02-18 00:16:53 +0800

    update

--- OS/真相还原/kernel/main.c
+++ OS/真相还原/kernel/main.c
@@ -1,11 +1,18 @@
 #include "print.h"
 int main(void) {
-    asm("xchg %bx,%bx");
-    // char buff[16] = "I am kernel\n";
-    put_str("I am kernel\n");
+    char* buff = "I am kernel\n";
+    // put_str("I am kernel\n");
+    put_str(buff);
+    char buff2[] = "I am kernel\n";
+    put_str(buff2);
+
+    // char buff3[20] = "I am kernel\n";
+    // put_str(buff3);
+    // asm("int3");
     put_char('A');
     put_int(9);
     put_char('\n');
+    asm("xchg %bx,%bx");
     put_int(0x12345678);
     put_char('\n');
     put_int(0x00000000);
