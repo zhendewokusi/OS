52050a1c8d671d80ab99a4febb8a3f83cdd53277
zhendewokusi <768672249@qq.com>
2024-02-20 23:35:01 +0800

    Sort

--- OS/真相还原/include/kernel/bitmap.h
+++ OS/真相还原/include/kernel/bitmap.h
@@ -1,5 +1,10 @@
 #ifndef __BITMAP_H__
 #define __BITMAP_H__
 
+#include "stdint.h"
+#define PAGE_SIZE 4096
+
+// 书上代码是让写4GB的，但是正常这里肯定要按照能分配最多的物理页来呀.....
+extern uint64_t mem_size;
 
 #endif
--- OS/真相还原/kernel/main.c
+++ OS/真相还原/kernel/main.c
@@ -1,3 +1,4 @@
+#include "stdint.h"
 #include "interrupt.h"
 #include "print.h"
 #include "init.h"
@@ -6,10 +7,13 @@
 #include "traps.h"
 #include "string.h"
 
-int mem_size = 0;
+
+uint64_t mem_size = 0x100000000;
 
 int main(void) {
         // 什么狗屎代码，直接操作内存拿loader获取的mem_size，头疼，之前写的loader没考虑到位，哎
+        put_int(mem_size);
+        put_char('\n');
         asm volatile (
         "movl $0xc009efec, %%eax\n\t"
         "movl (%%eax), %0"
@@ -17,6 +21,9 @@ int main(void) {
         :
         : "eax"
         );
+        put_int(mem_size);
+        put_char('\n');
+
         // 初始化
         init_all();
 
