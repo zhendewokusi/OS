64e64f3f7d144e40f3eccdf3245c8ade00ea0d45
zhendewokusi <768672249@qq.com>
2024-02-21 00:20:27 +0800

    走错方向了...绷

--- OS/真相还原/kernel/main.c
+++ OS/真相还原/kernel/main.c
@@ -11,19 +11,7 @@
 uint64_t mem_size = 0x100000000;
 
 int main(void) {
-        // 什么狗屎代码，直接操作内存拿loader获取的mem_size，头疼，之前写的loader没考虑到位，哎
-        put_int(mem_size);
-        put_char('\n');
-        asm volatile (
-        "movl $0xc009efec, %%eax\n\t"
-        "movl (%%eax), %0"
-        : "=r" (mem_size)
-        :
-        : "eax"
-        );
-        put_int(mem_size);
-        put_char('\n');
-
+        
         // 初始化
         init_all();
 
