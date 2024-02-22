6633957152b54992de1ca7db5c508f261a555106
zhendewokusi <768672249@qq.com>
2024-02-23 02:06:29 +0800

    update

--- OS/真相还原/include/kernel/memory.h
+++ OS/真相还原/include/kernel/memory.h
@@ -1,16 +1,17 @@
 #ifndef __MEMORY_H__
 #define  __MEMORY_H__
+#include <stdint.h>
 #include "stdint.h"
 #include "bitmap.h"
 
 enum pool_flags {
-   PF_KERNEL = 1,    // 内核内存池
-   PF_USER = 2	     // 用户内存池
+        PF_KERNEL = 0,    // 内核内存池
+        PF_USER = 1	      // 用户内存池
 };
 
 #define	 PG_P_1	  1	// 页表项或页目录项存在属性位
 #define	 PG_P_0	  0	// 页表项或页目录项存在属性位
-#define	 PG_RW_R  0	// R/W 属性位值, 读/执行
+#define	 PG_RW_R  0     // R/W 属性位值, 读/执行
 #define	 PG_RW_W  2	// R/W 属性位值, 读/写/执行
 #define	 PG_US_S  0	// U/S 属性位值, 系统级
 #define	 PG_US_U  4	// U/S 属性位值, 用户级
@@ -20,6 +21,13 @@ struct virtual_addr {
    struct bitmap vaddr_bitmap;
    uint32_t vaddr_start;
 };
+
+static void * vaddr_get(enum pool_flags pl_flag,uint32_t pg_cnt);
+// static void * palloc(struct memo_pool* mempool);
+// static void page_table_add(void* vaddr,void* pg_paddr);
+
+void * get_kernel_pge(uint32_t pg_cnt);
+void * malloc_page(enum pool_flags pl_flag,uint32_t pg_cnt);
 
 void memo_init();
 static void mem_pool_init(uint32_t all_mem);
--- OS/真相还原/kernel/memory.c
+++ OS/真相还原/kernel/memory.c
@@ -2,6 +2,9 @@
 #include "print.h"
 #include "stdint.h"
 #include "bitmap.h"
+#include "traps.h"
+#include "string.h"
+
 
 #define PAGE_SIZE 4096
 // 页表大小= 1页的页目录表+第0和第768个页目录项指向同一个页表+第769~1022个页目录项共指向254个页表,共256个页框
@@ -22,6 +25,10 @@
 
 // 我这把pool_size写死了，和bochs配置信息匹配（脚本）
 // 问题：我该咋写虚拟地址的位图qaq？头大了
+
+#define __PDE_INDEX__(addr) ((addr & 0xffc00000) >> 22)
+#define __PTE_INDEX__(addr) ((addr & 0x003ff000) >> 12)
+
 struct memo_pool {
         uint32_t phy_addr_start;
         struct bitmap pool_bitmap;
@@ -30,6 +37,75 @@ struct memo_pool {
 
 struct memo_pool user_pool,kernel_pool;
 struct virtual_addr kernel_vaddr;	 // 给内核分配虚拟地址使用
+
+
+// 从物理位图中找到一个空闲页并返回其地址
+static void * palloc(struct memo_pool* mempool)
+{
+        int bit_index = bitmap_scan(&mempool->pool_bitmap, 1);
+        if(bit_index == -1)   return NULL;
+        bitmap_set(&mempool->pool_bitmap,bit_index,1);
+        uint32_t pg_phyaddr = mempool->phy_addr_start + bit_index * PAGE_SIZE;
+        return (void *) pg_phyaddr;
+
+}
+
+static void * vaddr_get(enum pool_flags pl_flag,uint32_t pg_cnt)
+{
+        uint32_t cnt = 0;
+        int vaddr_start = 0, bit_idx_start = -1; //存储分配的起始虚拟地址和位图扫描函数bitmap_scan的返回值
+        if (pl_flag == PF_KERNEL) {
+           bit_idx_start  = bitmap_scan(&kernel_vaddr.vaddr_bitmap, pg_cnt);
+           if (bit_idx_start == -1) {
+              return NULL;
+           }
+           while(cnt < pg_cnt) {
+                bitmap_set(&kernel_vaddr.vaddr_bitmap, bit_idx_start + cnt++, 1);
+           }
+           vaddr_start = kernel_vaddr.vaddr_start + bit_idx_start * PAGE_SIZE;
+        } else {	
+        // 用户内存池,将来实现用户进程再补充
+        }
+        return (void*)vaddr_start;
+}
+
+
+void * malloc_page(enum pool_flags pl_flag,uint32_t pg_cnt)
+{
+        // 1. 通过vaddr_get在虚拟内存池中申请虚拟地址
+        // 2. 通过palloc在物理内存池中申请物理页
+        // 3. 通过page_table_add将以上得到的虚拟地址和物理地址在页表中完成映射
+        ASSERT(pl_flag == 0 || pl_flag == 1);
+        // 这里应该还要设置一个上限，不然直接申请完kernelMemory就无了
+        ASSERT(pg_cnt > 0);
+        void * vaddr_start = vaddr_get(PF_KERNEL,pg_cnt);
+        // 防止没有申请成功
+        if(vaddr_start == NULL) return NULL;
+        uint32_t vaddr = (uint32_t) vaddr_start;
+        struct memo_pool * mempool = pl_flag & PF_KERNEL ? &kernel_pool : &user_pool;
+        while (pg_cnt--) {
+                void * pg_paddr = palloc(mempool);
+                if(pg_paddr == NULL) return NULL;
+                // page_table_add((void*)vaddr,pg_paddr);
+                vaddr += PAGE_SIZE;
+        }
+        return vaddr_start;
+
+}
+
+void * get_kernel_pge(uint32_t pg_cnt)
+{
+        // 参数是无符号...
+        // ASSERT(pg_cnt > 0);
+        void * addr = malloc_page(PF_KERNEL,pg_cnt);
+        if (addr != NULL) {
+                memset(addr,0,pg_cnt * PAGE_SIZE);
+        }
+        return addr;
+}
+
+
+
 
 static void mem_pool_init(uint32_t all_mem)
 {
@@ -41,6 +117,7 @@ static void mem_pool_init(uint32_t all_mem)
         uint16_t all_free_pages = free_mem / PAGE_SIZE;
         uint16_t kernel_free_pages = all_free_pages / 2;
         uint16_t user_free_pages = all_free_pages - kernel_free_pages;
+        // 这里没有会少 0 ~ 14 物理页不在位图中 kernel一个 user一个
         uint32_t kbm_length = kernel_free_pages / 8;
         uint32_t ubm_length = user_free_pages / 8;
         uint32_t kp_start = used_mem;				  // Kernel Pool start,内核内存池的起始地址
